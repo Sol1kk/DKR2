485c286a6ad37a5c62674a7a715a18930f81ceee
Sol1kk <113501863+Sol1kk@users.noreply.github.com>
2022-11-09 14:17:09 +0300

    Add files via upload

---
+++ Лаба 2.pas
@@ -0,0 +1,23 @@
+﻿program lr2;
+var a:array[1..100] of integer;
+begin
+var b:=true;
+var n:=readinteger('Введите n:');
+for var i:=1 to n do
+    begin
+    Print('Введите a[',i,']:');
+   a[i]:=readinteger();
+    end;
+
+for var i:=2 to N-1 do
+if (a[i] < 0) and (a[i-1] >= 0) and (a[i+1] >= 0) then continue
+   else
+      if (a[i] >= 0) and (a[i-1] < 0) and (a[i+1] < 0) then continue
+            else begin
+                b := false;
+                break;
+            end;
+if b then Print('Знакочередующийся') else Print('Не знакочередующийся');
+begin
+end;
+end.
---
+++ лаба2(2задание).pas
@@ -0,0 +1,20 @@
+﻿begin
+ var s:string;
+ print('Строка=');
+ s:=readstring();
+ var e:=length(s);
+ var b:=0;
+ var c:=0;
+ for var i:=1 to e do
+ begin
+   if s[i] in ['0'..'9'] then inc(c);
+   case s[i] of
+   'a'..'z': inc(b);
+   'A'..'Z': inc(b);
+   'а'..'я': inc(b);
+   'А'..'Я': inc(b);
+   end;
+   end;
+   print('букв',b);
+   print('цифр',c)
+ end.
